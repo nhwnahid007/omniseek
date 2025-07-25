dc215bb0f1029262cd8fed6e4464fd044817f5e3
Md. Nahidul Islalm <nhwnahid@gmail.com>
2025-07-26 03:12:23 +0600

    added qr code

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -30,7 +30,11 @@ const Sidebar = ({ expand, setExpand }) => {
               alt=""
               className="hidden md:block w-7"
             />
-            <div className={`absolute w-max ${expand? 'left1/2 -translate-x-1/2 top-12': '-top-12 left-0'} opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+            <div
+              className={`absolute w-max ${
+                expand ? "left1/2 -translate-x-1/2 top-12" : "-top-12 left-0"
+              } opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}
+            >
               {expand ? "Close Sidebar" : "Open Sidebar"}
               <div
                 className={`w-3 h-3 absolute bg-black rotate-45 ${
@@ -43,17 +47,59 @@ const Sidebar = ({ expand, setExpand }) => {
           </div>
         </div>
 
-                  <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max': 'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
-                    <Image src={ expand? assets.chat_icon : assets.chat_icon_dull} alt="" className={expand? 'w-6':'w-7'}/>
-                    <div className="absolute w-max -top-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none">
-                      New Chat 
-                      <div className="w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5">
+        <button
+          className={`mt-8 flex items-center justify-center cursor-pointer ${
+            expand
+              ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max"
+              : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"
+          }`}
+        >
+          <Image
+            src={expand ? assets.chat_icon : assets.chat_icon_dull}
+            alt=""
+            className={expand ? "w-6" : "w-7"}
+          />
+          <div className="absolute w-max -top-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none">
+            New Chat
+            <div className="w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5"></div>
+          </div>
+          {expand && <p className="text-white text font-medium">New Chat </p>}
+        </button>
+        <div
+          className={`mt-8 text-white/25 text-sm ${
+            expand ? "block" : "hidden"
+          }`}
+        >
+          <p className="my-1">Recents</p>
+          {/* chat label */}
+        </div>
+      </div>
 
-                      </div>
-                    </div>
-                    {expand && <p className="text-white text font-medium">New Chat </p>}
-                  </button>
-
+      <div className="">
+        <div className={`flex items-center cursor-pointer group relative ${expand? 'gap-1 text-white/80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer': 'h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
+          <Image
+            className={expand ? "w-5" : "w-6.5 mx-auto"}
+            src={expand ? assets.phone_icon : assets.phone_icon_dull}
+            alt=""
+          />
+          <div className={`absolute -top-60 pb-8 ${!expand && "-right-40" } opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
+            <div className="relative w-max bg-black text-white text-sm p-3 rounded-lg shadow-lg">
+              <Image src={assets.qrcode} alt="" className="w-44" />
+              <p>Scan to get DeepSeek App</p>
+              <div
+                className={`w-3 h-3 absolute bg-black rotate-45 ${
+                  expand ? "right-1/2" : "left-4"
+                } -bottom-1.5`}
+              ></div>
+            </div>
+          </div>
+          {expand && (
+            <>
+              <span>Get App</span>
+              <Image src={assets.new_icon} alt="" />
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
